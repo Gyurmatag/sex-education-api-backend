6ae0106defdad4bdd3aed7682eafeafed4e8bc8b
Varga György <gyurmatag@gmail.com>
2020-12-04 23:06:29 +0100

    heroku redis konfigok

--- src/utils/init-redis.js
+++ src/utils/init-redis.js
@@ -1,7 +1,7 @@
 const redis = require('redis')
 class Redis {
   constructor () {
-    this.url = process.env.REDIS_URL
+    this.url = process.env.NODE_ENV === 'development' ? new URL(process.env.REDIS_URL) : new URL(process.env.REDISCLOUD_URL)
     this.connected = false
     this.client = null
   }
@@ -10,7 +10,8 @@ class Redis {
     if (this.connected) {
       return this.client
     } else {
-      this.client = redis.createClient(this.url)
+      const redisURL = this.url
+      this.client = redis.createClient(redisURL.port, redisURL.hostname, { no_ready_check: true })
       this.connected = true
       return this.client
     }
